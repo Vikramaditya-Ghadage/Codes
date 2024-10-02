219c76436f22b8f8d49b67e7dfb9e051de63016d
Vikramaditya Ghadage <54613065+Vikramaditya-Ghadage@users.noreply.github.com>
2024-10-02 15:46:13 +0530

    Update RemoveDuplicate.cpp

--- C++/SDE_SHEET/Array2/RemoveDuplicate.cpp
+++ C++/SDE_SHEET/Array2/RemoveDuplicate.cpp
@@ -1,3 +1,4 @@
+// Online C++ compiler to run C++ program online
 #include<bits/stdc++.h>
 using namespace std;
 // Time-Complixity: O(N) it's a Linear
@@ -17,8 +18,21 @@ void removeDuplicates(vector<int> &nums)
     cout<<i<<" ";
 
 }
+
+// Time-Complixity: O(N) it's a Linear
+void removeDuplicatesUsingSet(vector<int> &nums)
+{
+    unordered_set<int> st;
+    for(auto no:nums)
+    st.insert(no);
+    
+    for(auto i:st)
+    cout<<i<<" ";
+
+}
 int main()
 {
-    vector<int> nums={1,0,2,1,2,3,3,0};
-    removeDuplicates(nums);
+    vector<int> nums={1,0,2,1,2,3,3,0,4,5,5};
+   // removeDuplicates(nums);
+    removeDuplicatesUsingSet(nums);
 }
