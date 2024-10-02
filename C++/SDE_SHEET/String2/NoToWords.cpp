2362ab7a878bb1ed2661de34205804caba828b07
Vikramaditya Ghadage <54613065+Vikramaditya-Ghadage@users.noreply.github.com>
2024-10-02 23:06:21 +0530

    Update NoToWords.cpp

--- C++/SDE_SHEET/String2/NoToWords.cpp
+++ C++/SDE_SHEET/String2/NoToWords.cpp
@@ -1,49 +1,59 @@
-#include <bits/stdc++.h>	
+#include <bits/stdc++.h>
 using namespace std;
+
+// Arrays to store word equivalents of numbers
 string one[] = { "", "one ", "two ", "three ", "four ",
                  "five ", "six ", "seven ", "eight ",
                  "nine ", "ten ", "eleven ", "twelve ",
                  "thirteen ", "fourteen ", "fifteen ",
                  "sixteen ", "seventeen ", "eighteen ",
                  "nineteen " };
+
 string ten[] = { "", "", "twenty ", "thirty ", "forty ",
                  "fifty ", "sixty ", "seventy ", "eighty ",
-                 "ninety " };
-   
-string noToWord(long no,string s)
-{
+                 "ninety "};
+
+// Function to convert a number into words
+string noToWord(int no, string s) {
     string ans;
-    
-    if(no>19)
-    ans+=ten[no/10]+one[no%10];
+    if (no > 19)
+        ans += ten[no / 10] + one[no % 10];
     else
-    ans+=one[no];
-    
-    if(no)
-    ans+=s+" ";
-    
-    cout<<no<<":"<<ans<<endl;
-    
-    return ans;
-}
-string convertToString(long no)
-{
-    string ans;
-    
-    ans+=noToWord((no/10000000),"crore");
-    ans+=noToWord((no/100000)%100,"lakh");
-    ans+=noToWord((no/1000)%100,"thousand");
-    ans+=noToWord((no/100)%10,"hundred and");
-    
-    ans+=noToWord((no%100),"");
+        ans += one[no];
+
+    if (no)
+    ans += s;
     
     return ans;
 }
 
+// Function to convert a large number to words
+string convertToString(long no) {
+    string ans;
+
+    // Vectors for place values and their corresponding names
+    vector<int> num = {10000000, 100000, 1000, 100};
+    vector<string> place = {"crore ", "lakh ", "thousand ", "hundred "};
+
+    // Loop through the vectors to construct the result
+    for (int i = 0; i < num.size(); i++) {
+        ans += noToWord(no / num[i], place[i]);
+        no %= num[i];  // Update the number after each place value is processed
+    }
+
+    // Adding "and" if necessary (when there is a remainder below 100 and it's not part of a "hundred")
+    if (no > 0 && ans.size() > 0) {
+        ans += "and ";
+    }
+
+    // Process the last two digits (1s and 10s place)
+    ans += noToWord(no, "");
+
+    return ans;
+}
 
 int main() {
-    
-    long no=128836442;
-    cout<<convertToString(no);
+    long no = 128836442;  // Example number
+    cout << convertToString(no) << endl;
     return 0;
 }
